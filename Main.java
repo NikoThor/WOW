f0181a502d875569016f9b1a30b2d86d27f91ae4
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-09-10 13:10:51 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package Worldofwarcraft;
+
+public static void main(String[]args){
+
+        }
+public class Main {
+    WoW nealth = new WoW();
+
+    WoW pheonix = new WoW();
+
+    nealth.setfaction("Alliance")
+
+
+
+}
